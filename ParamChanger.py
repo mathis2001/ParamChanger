6645540624d14059afc9ed9606f7a33cc6d8dd4e
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-08-09 16:45:26 +0200

    Add files via upload

---
+++ ParamChanger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+	for line in sys.stdin:
+		try:
+			params = dict(x.split('=') for x in line.split('&'))
+			for key, value in params.items():
+				params[key]=sys.argv[1]
+			url = '&'.join('{}={}'.format(key, value) for key, value in params.items())
+			print(url)
+		except:
+			pass
+
+if __name__ == '__main__':
+	try:
+		main()
+	except Exception as e:
+		print("A problem has occured.")
+		print("Error info:")
+		print(e)
+	except KeyboardInterrupt:
+	       	print("Script canceled.")	
