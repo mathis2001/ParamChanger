c3cae77619ea4c53908541fd9958359db319db39
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-08-09 17:11:14 +0200

    Update ParamChanger.py

--- ParamChanger.py
+++ ParamChanger.py
@@ -3,15 +3,19 @@
 import sys
 
 def main():
-	for line in sys.stdin:
-		try:
-			params = dict(x.split('=') for x in line.split('&'))
-			for key, value in params.items():
-				params[key]=sys.argv[1]
-			url = '&'.join('{}={}'.format(key, value) for key, value in params.items())
-			print(url)
-		except:
-			pass
+	if len(sys.argv)<2:
+                print("usage: python3 ParamChanger.py <payload>")
+                sys.exit(1)
+        else:
+		for line in sys.stdin:
+			try:
+				params = dict(x.split('=') for x in line.split('&'))
+				for key, value in params.items():
+					params[key]=sys.argv[1]
+				url = '&'.join('{}={}'.format(key, value) for key, value in params.items())
+				print(url)
+			except:
+				pass
 
 if __name__ == '__main__':
 	try:
